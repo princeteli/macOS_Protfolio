42bcb34c00979629168ac21f6cd597b7cd58d6be
prince <pt0000100@gmail.com>
2026-08-10 08:34:21 +0530

    complete tirminal part

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import gsap from 'gsap'
+import { Draggable } from 'gsap/Draggable'
+
 import { Dock, Navbar, Welcome } from '#components'
-import React from 'react'
+import { Terminal } from '#windows'
 
 const App = () => {
+  gsap.registerPlugin(Draggable)
   return (
     <main>
       <Navbar/>
       <Welcome/>
       <Dock/>
+
+
+      <Terminal/>
     </main>
   )
 }
